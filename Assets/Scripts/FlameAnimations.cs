6a7b633a1ef78b5f75d25eb8ee041282630a5cc5
Jeff Polasz <jeff_polasz@hotmail.com>
2018-01-12 03:17:40 -0500

    Added Torch and flame animations

---
+++ Assets/Scripts/FlameAnimations.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlameAnimations : MonoBehaviour {
+
+	public int lightMode;
+	public GameObject flameLight;
+	
+	// Update is called once per frame
+	void Update () {
+		if (lightMode == 0) {
+			StartCoroutine (AnimateLight ());
+		}
+	}
+
+	IEnumerator AnimateLight () {
+		lightMode = Random.Range (1, 4);
+		if (lightMode == 1) {
+			flameLight.GetComponent<Animation> ().Play ("TorchAnim1");
+		}
+
+		if (lightMode == 2) {
+			flameLight.GetComponent<Animation> ().Play ("TorchAnim2");
+		}
+
+		if (lightMode == 3) {
+			flameLight.GetComponent<Animation> ().Play ("TorchAnim3");
+		}
+		yield return new WaitForSeconds(0.99f);
+		lightMode = 0;
+	}
+
+} // FlameAnimations
