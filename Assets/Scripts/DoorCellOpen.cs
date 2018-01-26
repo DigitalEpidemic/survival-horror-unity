a5bfcf6747531a1c8e263f157047e7cdd9a78c1a
Jeff Polasz <jeff_polasz@hotmail.com>
2018-01-26 18:27:24 -0500

    Added Action button + UI and door open with creak sound

---
+++ Assets/Scripts/DoorCellOpen.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DoorCellOpen : MonoBehaviour {
+
+	public float theDistance;
+	public GameObject actionDisplay;
+	public GameObject actionText;
+	public GameObject theDoor;
+	public AudioSource creakSound;
+
+	void Update () {
+		theDistance = PlayerCasting.distanceFromTarget;
+	}
+
+	void OnMouseOver () {
+		if (theDistance <= 2) {
+			actionDisplay.SetActive (true);
+			actionText.SetActive (true);
+		}
+
+		if (Input.GetButtonDown ("Action")) {
+			if (theDistance <= 2) {
+				this.GetComponent<BoxCollider> ().enabled = false;
+				actionDisplay.SetActive (false);
+				actionText.SetActive (false);
+				theDoor.GetComponent<Animation> ().Play ("FirstDoorOpenAnim");
+				creakSound.Play ();
+			}
+		}
+	}
+
+	void OnMouseExit () {
+		actionDisplay.SetActive (false);
+		actionText.SetActive (false);
+	}
+
+} // DoorCellOpen
