61e3b125748b7e281b3564bb7d303f7f439724a6
Jeff Polasz <jeff_polasz@hotmail.com>
2018-01-28 16:17:44 -0500

    Added opening sequence

---
+++ Assets/Scripts/Sequences/AOpening.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.Characters.FirstPerson;
+using UnityEngine.UI;
+
+public class AOpening : MonoBehaviour {
+
+	public GameObject thePlayer;
+	public GameObject fadeScreenIn;
+	public GameObject textBox;
+
+	void Start () {
+		thePlayer.GetComponent<FirstPersonController> ().enabled = false;
+		StartCoroutine (ScenePlayer ());
+	}
+
+	IEnumerator ScenePlayer () {
+		yield return new WaitForSeconds (1.5f);
+		fadeScreenIn.SetActive (false);
+		textBox.GetComponent<Text> ().text = "I need to get out of here.";
+
+		yield return new WaitForSeconds (2f);
+		textBox.GetComponent<Text> ().text = "";
+		thePlayer.GetComponent<FirstPersonController> ().enabled = true;
+	}
+
+} // AOpening
