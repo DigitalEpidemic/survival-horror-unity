7dc7eb6dc8a748ac54c637475423ee0bb7f547f7
Jeff Polasz <jeff_polasz@hotmail.com>
2018-01-28 16:56:28 -0500

    Imported table, extended level and added FirstTrigger

---
+++ Assets/Scripts/Sequences/BFirstTrigger.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.Characters.FirstPerson;
+using UnityEngine.UI;
+
+public class BFirstTrigger : MonoBehaviour {
+
+	public GameObject thePlayer;
+	public GameObject textBox;
+	public GameObject theMarker;
+
+	void OnTriggerEnter () {
+		// Pause player
+		thePlayer.GetComponent<FirstPersonController> ().enabled = false;
+		StartCoroutine (ScenePlayer ());
+	}
+
+	IEnumerator ScenePlayer () {
+		textBox.GetComponent<Text> ().text = "Looks like a weapon is on that table.";
+		yield return new WaitForSeconds (2.5f);
+		textBox.GetComponent<Text> ().text = "";
+		thePlayer.GetComponent<FirstPersonController> ().enabled = true;
+		theMarker.SetActive (true);
+	}
+
+} // BFirstTrigger
